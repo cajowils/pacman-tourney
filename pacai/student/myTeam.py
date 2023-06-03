a02bedce51de52bf96d9d80449af4eadaa58e58e
edchwong <73096416+edchwong@users.noreply.github.com>
2023-06-03 00:45:27 -0700

    offensive: density 300 distanceToFood -10 defensive: reverse -10
    also styled the code

--- pacai/student/myTeam.py
+++ pacai/student/myTeam.py
@@ -1,12 +1,18 @@
 from pacai.util import reflection
+
 # from pacai.agents.capture.capture import CaptureAgent
 from pacai.agents.capture.reflex import ReflexCaptureAgent
 from pacai.core.directions import Directions
 from pacai.core import distance
 
-def createTeam(firstIndex, secondIndex, isRed,
-        first = 'pacai.student.myTeam.OffensiveAgent',
-        second = 'pacai.student.myTeam.DefensiveAgent'):
+
+def createTeam(
+    firstIndex,
+    secondIndex,
+    isRed,
+    first="pacai.student.myTeam.OffensiveAgent",
+    second="pacai.student.myTeam.DefensiveAgent",
+):
     """
     This function should return a list of two agents that will form the capture team,
     initialized using firstIndex and secondIndex as their agent indexed.
@@ -17,10 +23,8 @@ def createTeam(firstIndex, secondIndex, isRed,
     firstAgent = reflection.qualifiedImport(first)
     secondAgent = reflection.qualifiedImport(second)
 
-    return [
-        firstAgent(firstIndex),
-        secondAgent(secondIndex)
-    ]
+    return [firstAgent(firstIndex), secondAgent(secondIndex)]
+
 
 class OffensiveAgent(ReflexCaptureAgent):
     """
@@ -54,43 +58,47 @@ class OffensiveAgent(ReflexCaptureAgent):
     def getFeatures(self, gameState, action):
         features = {}
         successor = self.getSuccessor(gameState, action)
-        features['successorScore'] = self.getScore(successor)
+        features["successorScore"] = self.getScore(successor)
 
         # Compute distance to the nearest food.
         foodList = self.getFood(successor).asList()
 
         # This should always be True, but better safe than sorry.
-        if (len(foodList) > 0):
+        if len(foodList) > 0:
             myPos = successor.getAgentState(self.index).getPosition()
             minDistance = min([self.getMazeDistance(myPos, food) for food in foodList])
-            features['remainingFood'] = len(foodList)
-            features['distanceToFood'] = minDistance
-            features['densityFood'] = self.calculateDensityValue(successor, False)
+            features["remainingFood"] = len(foodList)
+            features["distanceToFood"] = minDistance
+            features["densityFood"] = self.calculateDensityValue(successor, False)
         # Discourage stop action
-        if (action == Directions.STOP):
-            features['stop'] = 1
+        if action == Directions.STOP:
+            features["stop"] = 1
 
         # Discourage moving backwards
         rev = Directions.REVERSE[gameState.getAgentState(self.index).getDirection()]
-        if (action == rev):
-            features['reverse'] = 1
+        if action == rev:
+            features["reverse"] = 1
 
         # Calculate the distance to the closest capsule
         capsuleList = self.getCapsules(gameState)
         minCapsule = -100000
         if capsuleList:
-            minCapsule = min([self.getMazeDistance(myPos, capsule) for capsule in capsuleList])
+            minCapsule = min(
+                [self.getMazeDistance(myPos, capsule) for capsule in capsuleList]
+            )
 
         # Set feature value for closest capsule if capsule exists in game
         if minCapsule != -100000:
             if minCapsule == 0:
-                features['distanceToCapsules'] = -100
+                features["distanceToCapsules"] = -100
             else:
-                features['distanceToCapsules'] = minCapsule
+                features["distanceToCapsules"] = minCapsule
 
         # Initialize variables for computing defenders and invaders feature
         enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
-        defenders = [a for a in enemies if not a.isPacman() and a.getPosition() is not None]
+        defenders = [
+            a for a in enemies if not a.isPacman() and a.getPosition() is not None
+        ]
         invaders = [a for a in enemies if a.isPacman() and a.getPosition() is not None]
         normalVal = 0
         scaredVal = 0
@@ -109,9 +117,9 @@ class OffensiveAgent(ReflexCaptureAgent):
         if normal:
             distsNormal = [self.getMazeDistance(myPos, a.getPosition()) for a in normal]
             normalVal = min(distsNormal)
-            features['normalValue'] = normalVal
+            features["normalValue"] = normalVal
             if normalVal <= 1:
-                features['normalValue'] = -100000
+                features["normalValue"] = -100000
 
         # Calculate minimum distance to scared enemy defending agents
         if scared:
@@ -120,19 +128,21 @@ class OffensiveAgent(ReflexCaptureAgent):
 
             # Update scaredValue if fits requirements
             if scaredVal == 0:
-                features['scaredValue'] = 1
+                features["scaredValue"] = 1
 
         # Calculate the invaderValue
         if invaders:
-            distsInvaders = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders]
-            features['invaderValue'] = min(distsInvaders)
+            distsInvaders = [
+                self.getMazeDistance(myPos, a.getPosition()) for a in invaders
+            ]
+            features["invaderValue"] = min(distsInvaders)
 
         # Include numInvaders so it would kill enemy pacman when they're invading
-        features['numInvaders'] = len(invaders)
+        features["numInvaders"] = len(invaders)
 
         return features
 
-    def getDensityDict(self, gameState, d = False):
+    def getDensityDict(self, gameState, d=False):
         # Initialize variables to get the density
         foodGrid = self.getFood(gameState)
         defendFood = self.getFoodYouAreDefending(gameState)
@@ -148,7 +158,6 @@ class OffensiveAgent(ReflexCaptureAgent):
 
         # Iterate through each food in the foodList
         for x1, y1 in foodList:
-
             # Set boundaries
             minX = max(1, x1 - 3)
             maxX = min(bounds[0], x1 + 3)
@@ -158,7 +167,7 @@ class OffensiveAgent(ReflexCaptureAgent):
             # Iterate through the x and y range and see if food exists
             foodCount = 0
             for x2 in range(minX, maxX):
-                for y2  in range(minY, maxY):
+                for y2 in range(minY, maxY):
                     if x2 < bounds[0] and x2 > 0 and y2 < bounds[1] and y2 > 0:
                         if d and defendFood[x2][y2]:
                             foodCount += 1
@@ -172,8 +181,7 @@ class OffensiveAgent(ReflexCaptureAgent):
         # near the key's position.
         return densities
 
-    def calculateDensityValue(self, gameState, d = False):
-
+    def calculateDensityValue(self, gameState, d=False):
         # Get densities dictionary, position, and initialize returned value
         densities = self.getDensityDict(gameState, d)
         densityCalculation = 0
@@ -194,18 +202,19 @@ class OffensiveAgent(ReflexCaptureAgent):
 
         # return weights
         return {
-            'successorScore': 200,
-            'invaderValue': -50,
-            'numInvaders': -1000,
-            'distanceToFood': -1,
-            'remainingFood': -1,
-            'distanceToCapsules': -2,
-            'stop': -100,
-            'reverse': -20,
-            'normalValue': 2,
-            'scaredValue': 100000,
-            'densityFood': 100
+            "successorScore": 200,
+            "invaderValue": -50,
+            "numInvaders": -1000,
+            "distanceToFood": -10,
+            "remainingFood": -1,
+            "distanceToCapsules": -2,
+            "stop": -100,
+            "reverse": -20,
+            "normalValue": 2,
+            "scaredValue": 100000,
+            "densityFood": 300,
         }
+
 
 class DefensiveAgent(ReflexCaptureAgent):
     """
@@ -257,32 +266,32 @@ class DefensiveAgent(ReflexCaptureAgent):
         myPos = myState.getPosition()
 
         # Computes whether we're on defense (1) or offense (0).
-        features['onDefense'] = 1
-        if (myState.isPacman()):
-            features['onDefense'] = 0
+        features["onDefense"] = 1
+        if myState.isPacman():
+            features["onDefense"] = 0
 
         # Computes distance to invaders we can see.
         enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
         invaders = [a for a in enemies if a.isPacman() and a.getPosition() is not None]
-        features['numInvaders'] = len(invaders)
+        features["numInvaders"] = len(invaders)
 
-        if (len(invaders) > 0):
+        if len(invaders) > 0:
             dists = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders]
-            features['invaderDistance'] = min(dists)
+            features["invaderDistance"] = min(dists)
         else:
             if self.edge and myPos == self.edges[0]:
                 self.edge = False
-                features['edge'] = self.getMazeDistance(myPos, self.edges[0])
+                features["edge"] = self.getMazeDistance(myPos, self.edges[0])
             elif not self.edge and myPos == self.edges[1]:
                 self.edge = True
-                features['edge'] = self.getMazeDistance(myPos, self.edges[1])
+                features["edge"] = self.getMazeDistance(myPos, self.edges[1])
 
-        if (action == Directions.STOP):
-            features['stop'] = 1
+        if action == Directions.STOP:
+            features["stop"] = 1
 
         rev = Directions.REVERSE[gameState.getAgentState(self.index).getDirection()]
-        if (action == rev):
-            features['reverse'] = 1
+        if action == rev:
+            features["reverse"] = 1
 
         # return features
         return features
@@ -294,10 +303,10 @@ class DefensiveAgent(ReflexCaptureAgent):
 
         # return weights
         return {
-            'numInvaders': -1000,
-            'onDefense': 100,
-            'invaderDistance': -100,
-            'stop': -100,
-            'reverse': -20,
-            'edge': -10
+            "numInvaders": -1000,
+            "onDefense": 100,
+            "invaderDistance": -200,
+            "stop": -100,
+            "reverse": -10,
+            "edge": -10,
         }
